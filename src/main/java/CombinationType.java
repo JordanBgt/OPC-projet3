2a880ff11cda92a3238412340776e4e49af94396
Bgt <jordanbouget@gmail.com>
2019-10-17 16:58:32 +0200

    Create Enum CombinationType
    SECRET, PROPOSED, ANSWERED

---
+++ src/main/java/CombinationType.java
@@ -0,0 +1,12 @@
+/**
+ * <b>Enum which represents the type of the combination</b>
+ *
+ * <p>Secret for the combination to make guess</p>
+ * <p>Proposed for a guess try</p>
+ * <p>Answered for the response to the proposed combination</p>
+ */
+public enum CombinationType {
+    SECRET,
+    PROPOSED,
+    ANSWERED
+}
