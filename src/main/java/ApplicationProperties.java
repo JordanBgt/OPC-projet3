d536a7c632bebe87cd7bfd5fd144d15ac76e539e
Bgt <jordanbouget@gmail.com>
2019-10-17 17:00:10 +0200

    Create Enum singleton ApplicationProperties
    To read application.properties file and get properties

---
+++ src/main/java/ApplicationProperties.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * <b>Enum singleton ApplicationProperties</b>
+ *
+ * To read application.properties file
+ */
+public enum ApplicationProperties {
+
+    INSTANCE;
+
+    private final Properties properties;
+
+    /**
+     * properties => list of properties
+     * properties.load => read a propertiy list
+     * getRessourceAsStream => returns an input stream for reading the specified file
+     */
+    ApplicationProperties(){
+        properties = new Properties();
+        try{
+            properties.load(getClass().getClassLoader().getResourceAsStream("application.properties"));
+        } catch (IOException e){
+            // TODO : logger l'exception
+        }
+    }
+
+    /**
+     * @return tryoutNumber property
+     */
+    public Integer getPropertyTryoutNumber(){
+        return Integer.parseInt(properties.getProperty("tryoutNumber"));
+    }
+
+    /**
+     * @return devMode property
+     */
+    public String getPropertyDevMode(){
+        return properties.getProperty("devMode");
+    }
+
+    /**
+     * @return totalCombinationNumber property
+     */
+    public Integer getPropertyTotalCombinationNumber(){
+        return Integer.parseInt(properties.getProperty("totalCombinationNumber"));
+    }
+
+    /**
+     * @return maxCombinationValue property
+     */
+    public Integer getPropertyMaxCombinationValue(){
+        return Integer.parseInt(properties.getProperty("maxCombinationValue"));
+    }
+}
