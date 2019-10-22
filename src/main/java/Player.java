28b39e1c7bc1ec5b2dc0afc7816f5effc646b271
jordan <jordanbouget@gmail.com>
2019-10-22 16:21:48 +0200

    Update Player Class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -12,6 +13,11 @@ import java.util.Scanner;
 public class Player extends Actor {
 
     /**
+     * Scanner to read user's entries
+     */
+    private Scanner sc = new Scanner(System.in);
+
+    /**
      * Ask the player to set an answer to the challenger's proposed combination
      * Check, for each proposed combination's values, if they are greater, less or equals to secret combination's values
      *
@@ -22,27 +28,37 @@ public class Player extends Actor {
     public void setAnsweredCombination(Combination challengerCombination) {
         System.out.println("Yours secret combination : "+this.secretCombination.getCombinationValues());
         System.out.println("Challenger's combination : "+challengerCombination.getCombinationValues());
-        System.out.println("Please check Challenger's combination : \"+\" if secret number is greater, " +
-                "\"-\" if secret number is less, or \"=\" if both numbers are equals. Separate each value with \",\". Example : \"+,-,-,=\"");
-        Scanner sc = new Scanner(System.in);
-        String entry = sc.nextLine();
-        String[] str = entry.split(",");
-        for (String s : str) {
-            this.answeredCombination.addCombinationValue(s);
-        }
+        this.askPlayerToSetCombinationValues(this.answeredCombination);
     }
 
     /**
-     * Ask the player to set a proposed combination to guess the secret combination
+     * Ask the player to set a proposed combination in order to guess the secret combination
      */
     @Override
-    public void setProposedCombination() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Key in a "+Combination.COMBINATION_SIZE+" numbers combination (between 0 and 9), separates with \",\"");
+    public void setProposedCombination(Combination answeredCombination) {
+        if((answeredCombination.getCombinationValues() != null) && (!answeredCombination.getCombinationValues().isEmpty())){
+            System.out.println("AI's answered combination : " + answeredCombination.getCombinationValues());
+        }
+        this.askPlayerToSetCombinationValues(this.proposedCombination);
+    }
+
+    /**
+     * Ask the player to set a secret combination
+     */
+    public void setSecretCombination() {
+        this.askPlayerToSetCombinationValues(this.secretCombination);
+    }
+
+    /**
+     * It fills values of a combination with player's entries
+     *
+     * @param combination
+     *          player combination : secret, proposed or answered
+     */
+    public void askPlayerToSetCombinationValues(Combination combination){
+        System.out.println(combination.getCombinationType().getMessage());
         String entry = sc.nextLine();
         String[] entryValues = entry.split(",");
-        for (String s : entryValues){
-            this.proposedCombination.addCombinationValue(s);
-        }
+        combination.setCombinationValues(Arrays.asList(entryValues));
     }
 }
