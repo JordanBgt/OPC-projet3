62383aaf2fe57e2693d7701bc352abb86b97fc94
jordan <jordanbouget@gmail.com>
2019-10-22 16:38:57 +0200

    Update CombinationType enum Add a message for each CombinationType

--- src/main/java/CombinationType.java
+++ src/main/java/CombinationType.java
@@ -1,12 +1,30 @@
 /**
- * <b>Enum which represents the type of the combination</b>
+ * <b>Enum which represents the type of the combination and its message</b>
  *
  * <p>Secret for the combination to make guess</p>
  * <p>Proposed for a guess try</p>
  * <p>Answered for the response to the proposed combination</p>
  */
 public enum CombinationType {
-    SECRET,
-    PROPOSED,
-    ANSWERED
+
+    SECRET ("Key in secret combination : " + ApplicationProperties.INSTANCE.getPropertyCombinationSize() +
+            " numbers between " + ApplicationProperties.INSTANCE.getPropertyMinCombinationValue() + " and " +
+            ApplicationProperties.INSTANCE.getPropertyMaxCombinationValue() + ", separates with \",\""),
+
+    PROPOSED ("Key in proposed combination : " + ApplicationProperties.INSTANCE.getPropertyCombinationSize() +
+            " numbers between " + ApplicationProperties.INSTANCE.getPropertyMinCombinationValue() + " and " +
+            ApplicationProperties.INSTANCE.getPropertyMaxCombinationValue() + ", separates with \",\""),
+
+    ANSWERED ("Please check Challenger's combination : \"+\" if secret number is greater, \"-\" if it's less" +
+            "or \"=\" if both numbers are equals. Separate each values with \",\". Example : \"+,-,-,=\"");
+
+    CombinationType(String message){
+        this.message = message;
+    }
+
+    private String message;
+
+    public String getMessage(){
+        return message;
+    }
 }
