844a8a7336a7f4292130b92d69370fcc151178e4
jordan <jordanbouget@gmail.com>
2019-10-31 12:14:29 +0100

    Update DuelMode

--- src/main/java/DuelMode.java
+++ src/main/java/DuelMode.java
@@ -4,44 +4,53 @@ public class DuelMode extends GameMode {
 
     private Player player;
     private ArtificialIntelligence artificialIntelligence;
-    private Logger logger = Logger.getLogger(DuelMode.class);
+    private static final Logger LOGGER = Logger.getLogger(DuelMode.class);
+    private String winner = "AI";
 
-    public DuelMode(){}
-
-    public DuelMode(Player player, ArtificialIntelligence artificialIntelligence) {
-        this.player = player;
-        this.artificialIntelligence = artificialIntelligence;
+    public DuelMode() {
+        this.player = new Player();
+        this.artificialIntelligence = new ArtificialIntelligence();
+        this.gameModeMessage = "Welcome to Duel Mode. To win : guess AI' secret combination before it !";
     }
 
     @Override
     public void play() {
+        System.out.println(this.getGameModeMessage());
         this.player.setSecretCombination();
-        logger.info("player secret combination : " + this.player.getSecretCombination().getCombinationValues());
+        System.out.println("player secret combination : " + this.player.getSecretCombination().getCombinationValues());
         this.artificialIntelligence.setSecretCombination();
-        logger.info("ai secret combination : " + this.artificialIntelligence.getSecretCombination().getCombinationValues());
-
+        if(isDevMode()){
+            LOGGER.info("AI' secret combination : " + this.artificialIntelligence.getSecretCombination().getCombinationValues());
+        }
         do{
             this.setProposedCombinations();
             this.setAnsweredCombination();
-        } while((!this.isResolve(this.player.getProposedCombination(), this.artificialIntelligence.getSecretCombination())) && (!this.isResolve(this.artificialIntelligence.getProposedCombination(), this.player.getSecretCombination())));
-
+        } while(!this.isResolve());
+        System.out.println("Game over, " + this.winner + " wins !");
+        System.out.println("AI' secret combination was : " + this.artificialIntelligence.getSecretCombination().getCombinationValues());
+        System.out.println("Player' secret combination was : " + this.player.getSecretCombination().getCombinationValues());
     }
 
     public void setProposedCombinations(){
         this.player.setProposedCombination(this.artificialIntelligence.getAnsweredCombination());
-        logger.info("player proposed combination : " + this.player.getProposedCombination().getCombinationValues());
-        if((this.player.getAnsweredCombination().getCombinationValues() != null) && (!this.player.getAnsweredCombination().getCombinationValues().isEmpty())){
-            this.artificialIntelligence.setProposedCombination(this.player.getAnsweredCombination());
-        } else {
-            this.artificialIntelligence.setProposedCombination(this.player.getAnsweredCombination());
+        checkIfCombinationAreEquals(this.player.getProposedCombination(), this.artificialIntelligence.getSecretCombination());
+        if(!this.isResolve()) {
+            if ((this.player.getAnsweredCombination().getCombinationValues() != null) && (!this.player.getAnsweredCombination().getCombinationValues().isEmpty())) {
+                this.artificialIntelligence.setProposedCombination(this.player.getAnsweredCombination());
+            } else {
+                this.artificialIntelligence.setProposedCombination(this.player.getAnsweredCombination());
+            }
+            checkIfCombinationAreEquals(this.artificialIntelligence.getProposedCombination(), this.player.getSecretCombination());
         }
-        logger.info("ai proposed combination : " + this.artificialIntelligence.getProposedCombination().getCombinationValues());
+        else {
+            this.winner = "Player";
+        }
     }
     
     public void setAnsweredCombination(){
-        this.player.setAnsweredCombination(this.artificialIntelligence.getProposedCombination());
-        logger.info("player answered combination : " + this.player.getAnsweredCombination().getCombinationValues());
-        this.artificialIntelligence.setAnsweredCombination(this.player.getProposedCombination());
-        logger.info("ai answered combination : " + this.artificialIntelligence.getAnsweredCombination().getCombinationValues());
+        if(!isResolve()) {
+            this.player.setAnsweredCombination(this.artificialIntelligence.getProposedCombination());
+            this.artificialIntelligence.setAnsweredCombination(this.player.getProposedCombination());
+        }
     }
 }
